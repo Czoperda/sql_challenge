ae454ade24f5f7581f6980b939a758b8db4bfc5b
Czoperda <125833924+Czoperda@users.noreply.github.com>
2024-02-29 18:43:59 -0500

    Add files via upload

---
+++ sql_challenge_tbl_schemata.sql
@@ -0,0 +1,52 @@
+
+CREATE TABLE salaries (
+	emp_no INT,
+	salary INT,
+	PRIMARY KEY (emp_no)
+	);
+	
+CREATE TABLE titles (
+	title_id VARCHAR(5),
+	title VARCHAR(30),
+	PRIMARY KEY (title_id)
+	);
+	
+DROP TABLE departments
+CREATE TABLE departments (
+	dept_no VARCHAR(4),
+	dept_name VARCHAR(30),
+	PRIMARY KEY (dept_no)
+	);
+	
+CREATE TABLE employees (
+	emp_no INT,
+	emp_title_id VARCHAR(5),
+	birth_date DATE,
+	first_name VARCHAR(25),
+	last_name VARCHAR(25),
+	sex VARCHAR(1),
+	hire_date DATE,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY (emp_no) REFERENCES salaries(emp_no),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id)
+	);
+ select * from employees;
+
+CREATE TABLE dept_emp (
+	emp_no INT,
+	dept_no VARCHAR(4),
+	PRIMARY KEY (emp_no, dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+ );
+select * from dept_emp;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(4),
+	emp_no INT ,
+	PRIMARY KEY (dept_no,emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+select * from dept_manager;
+
+
